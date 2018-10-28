83209d86a49ab0a1bc91d71346d6a6a82ede61a5
abdulrehman1937 <44475042+abdulrehman1937@users.noreply.github.com>
2018-10-28 16:52:43 +0500

    now goin to add status

--- src/classes/Book_loan.java
+++ src/classes/Book_loan.java
@@ -15,7 +15,7 @@ import java.util.Date;
  */
 public class Book_loan {
     private Users_class loaner;
-    
+    private int fine;
     private Date issuedate;
     private Date returndate;
     private Date returneddate;
@@ -28,19 +28,21 @@ public class Book_loan {
         returndate=null;
         returneddate=null;
         loaned_book=null;
+        fine=0;
     }
     public Book_loan(Users_class loaner,Book loaned_book)
     {
+        fine=0;
         LocalDate date=LocalDate.now();
-        this.returneddate=null;
+        this.returneddate=new Date(1899,11,11);
         this.loaner=loaner;
         this.loaned_book=loaned_book;
         this.issuedate=new Date(date.getYear(),date.getMonthValue(),date.getDayOfMonth());
         this.returndate=new Date(date.getYear(),date.getMonthValue(),date.getDayOfMonth()+7);
     }
-    public Book_loan(Users_class loaner,Book loaned_book,Date issuedate,Date retdate,Date returneddate)
+    public Book_loan(Users_class loaner,Book loaned_book,Date issuedate,Date retdate,Date returneddate,int fine)
     {
-        
+        this.fine=fine;
         this.returneddate=returneddate;
         this.loaner=loaner;
         this.loaned_book=loaned_book;
@@ -57,15 +59,44 @@ public class Book_loan {
     }
     public String getret()
     {
+        
         return returndate.toString();
+    }
+    public String getreted()
+    {
+        if(returneddate.getYear()==1899)
+        {
+            return "Not returned";
+        }
+        return returneddate.toString();
     }
     public void setActualDate()
     {
         LocalDate date=LocalDate.now();
         this.returneddate=new Date(date.getYear(),date.getMonthValue(),date.getDayOfMonth());
+        this.fine=this.myfine();
     }
-    
-    
+    public int myfine()
+    {
+        
+        LocalDate date=LocalDate.now();
+        Date today=new Date(date.getYear(),date.getMonthValue(),date.getDayOfMonth());
+        long diff=(today.getTime()-this.returndate.getTime())/86400000;
+        int days=(int)diff;
+        if(days>0)
+        {
+            return days*100;
+        }
+        return 0;
+    }
+    public int getfine()
+    {
+        return this.fine;
+    }
+    public Users_class getuser()
+    {
+        return this.loaner;
+    }
     
     
 }
